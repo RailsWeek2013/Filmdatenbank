b9fc1934a4297bce1980034cd1a199a5564c764f
Alexander Arnold <alexander.arnold@mni.thm.de>
2013-08-15 14:47:47 +0200

    Migration hinzugefügt

--- RubyMovies/db/migrate/20130815103551_create_reviews.rb
+++ RubyMovies/db/migrate/20130815103551_create_reviews.rb
@@ -1,7 +1,5 @@
 class CreateReviews < ActiveRecord::Migration
   def change
-  	drop_table :film_reviews
-  	drop_table :reviews
     create_table :reviews do |t|
       t.integer :note
       t.belongs_to :film
