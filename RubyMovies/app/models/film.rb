ad86ce54c98e148d33260841af8a7ab830c42956
Michael Lüttebrandt <michael.luettebrandt@mni.thm.de>
2013-08-15 12:52:16 +0200

    adds film model

---
+++ RubyMovies/app/models/film.rb
@@ -0,0 +1,9 @@
+class Film < ActiveRecord::Base
+	has_many :film_participants
+	has_many :participants, through: :film_participants
+	has_many :reviews
+	has_many :users, through: :reviews
+	has_many :film_tags
+	has_many :tags, through: :film_tags
+	has_many :comments
+end
