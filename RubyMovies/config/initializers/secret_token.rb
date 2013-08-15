fad636e266e785b98ccba338a999c64bc9abf5fd
Michael Lüttebrandt <michael.luettebrandt@mni.thm.de>
2013-08-15 09:58:55 +0200

    Projekt erstellt, Projektbeschreibung erstellt

---
+++ RubyMovies/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyMovies::Application.config.secret_key_base = '89768453f05d655c1c7c8100c7b4d554c5c681b4350676789312ce3c38cffb861e904632bf6236b5362f7e73f9e6bc24d3756abc00ad659e3eba042874dda779'
