1601aeb64004ad3df17a5faa8cb94dee0f0ff3d3
Michael Lüttebrandt <michael.luettebrandt@mni.thm.de>
2013-08-16 11:18:44 +0200

    Devise hinzugefügt

--- RubyMovies/config/routes.rb
+++ RubyMovies/config/routes.rb
@@ -1,6 +1,7 @@
 RubyMovies::Application.routes.draw do
+  devise_for :users
   resources :films
-
+  root :to => "films#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
