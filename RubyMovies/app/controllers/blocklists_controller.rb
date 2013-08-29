78fed243ae8788d11e818e2e191e4e0e08a83a07
Michael Lüttebrandt <michael.luettebrandt@mni.thm.de>
2013-08-29 11:47:05 +0200

    Refactoring Blocklist

--- RubyMovies/app/controllers/blocklists_controller.rb
+++ RubyMovies/app/controllers/blocklists_controller.rb
@@ -3,15 +3,11 @@ class BlocklistsController < ApplicationController
 	authorize_resource
 
 	def index
-		values = Blocklist.where(user_id: current_user)
-		@users = Array.new
-		values.each do |blocklist|
-			@users.push User.find(blocklist.blocked_id)
-		end
+		@users = Blocklist.get_blocked_users current_user
 	end
 
 	def destroy
-		Blocklist.where(user_id: current_user.id, blocked_id: params[:blocked_user]).first.destroy
+		Blocklist.where(user_id: current_user.id, blocked_id: params[:blocked_user]).take.destroy
 		redirect_to blocklist_path, notice: "Eintrag erfolgreich entfernt."
 	end
 
--- RubyMovies/app/models/blocklist.rb
+++ RubyMovies/app/models/blocklist.rb
@@ -1,8 +1,16 @@
 class Blocklist < ActiveRecord::Base
 
 	validates :user_id, :blocked_id, presence: true
-	#validates :user_id, uniqueness: { scope: :blocked_id}
 
 	validates_uniqueness_of :user_id, :scope => :blocked_id
+
+	def self.get_blocked_users user
+		blocklist = Blocklist.where(user_id: user)
+		users = Array.new
+		blocklist.each do |blocked_user|
+			users.push User.find(blocked_user.blocked_id)
+		end
+		users
+	end
 
 end
